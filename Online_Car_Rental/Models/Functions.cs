06d4fbea5ff9be1f8f930c477684971324c46548
Kosha <21ceuog040@ddu.ac.in>
2024-06-11 21:16:27 +0530

    Add project files.

---
+++ Online_Car_Rental/Models/Functions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Diagnostics.PerformanceData;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace Online_Car_Rental.Models
+{
+    public class Functions
+    {
+        public SqlConnection Conn;
+        private SqlCommand cmd;
+        private DataTable dt;
+        private string ConnStr;
+        private SqlDataAdapter sda;
+
+        public Functions()
+        {
+            ConnStr = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\Users\kosha\OneDrive\Documents\CarRentalDbASP.mdf; Integrated Security = True; Connect Timeout = 30";
+           // ConnStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\Sneha\OneDrive\Desktop\5_WDN\Online_Car_Rental_1 (5)\Online_Car_Rental_1\Online_Car_Rental\Online_Car_Rental\App_Data\newdatabase.mdf"";Integrated Security=True";
+            Conn = new SqlConnection(ConnStr);
+            cmd = new SqlCommand();
+            cmd.Connection = Conn;
+        }
+        public DataTable GetData(string Query)
+        {
+            dt = new DataTable();
+            sda = new SqlDataAdapter(Query, ConnStr);
+            sda.Fill(dt);
+            return dt;
+
+        }
+        public int SetData(string Query)
+        {
+            int rcnt = 0;
+            if(Conn.State == ConnectionState.Closed)
+            {
+                Conn.Open();
+            }
+            cmd.CommandText = Query;
+            rcnt = cmd.ExecuteNonQuery();
+            
+            Conn.Close();
+            return rcnt;
+        }
+    }
+}
---
+++ Online_Car_Rental/View/Admin/Rents.aspx.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Online_Car_Rental.View.Admin
+{
+    public partial class Rents : System.Web.UI.Page
+    {
+        Models.Functions Conn;
+        public override void VerifyRenderingInServerForm(Control control)
+        {
+            base.VerifyRenderingInServerForm(control);
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Conn = new Models.Functions();
+            ShowRents();
+        }
+
+        private void ShowRents()
+        {
+
+            string Query = "select * from RentTb1";
+            RentList.DataSource = Conn.GetData(Query);
+            RentList.DataBind();
+        }
+       
+
+        private void UpdateCar()
+        {
+            try
+            {
+
+                string Status = "Available";
+                string Query = "update CarTb1 set  Status = '{0}' where CplateNum = '{1}'";
+                Query = String.Format(Query, Status, RentList.SelectedRow.Cells[2].Text);
+
+                Conn.SetData(Query);
+                //ShowRents();
+                //  ErrorMsg.InnerText = "Car Edited";
+            }
+            catch (Exception Ex)
+            {
+                
+                InfoMsg.InnerText = Ex.Message;
+            }
+        }
+
+        private void ReturnCar()
+        {
+            try
+            {
+              
+                    string Query = "Delete from RentTb1 where RentId={0}";
+                    Query = String.Format(Query, RentList.SelectedRow.Cells[1].Text);
+
+                    Conn.SetData(Query);
+
+                ShowRents();
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            
+        }
+
+        string LicensePlate;
+        protected void RentList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LicensePlate = RentList.SelectedRow.Cells[1].Text;
+        }
+
+        
+
+        protected void BookBtn_Click1(object sender, EventArgs e)
+        {
+            try
+            {
+                if (RentList.SelectedRow.Cells[1].Text == "")
+                {
+                    InfoMsg.InnerText = "Select a Rental";
+                }
+                else
+                {
+
+                    string Query = "insert into ReturnTb1 values('{0}', {1}, '{2}', {3}, {4})";
+                    Query = String.Format(Query, RentList.SelectedRow.Cells[2].Text, RentList.SelectedRow.Cells[3].Text, DateTime.Now.ToString("dd-MM-yyyy"), DelayTb.Value, FineTb.Value);
+                    UpdateCar();
+                    ShowRents();
+                    Conn.SetData(Query);
+                    
+                    ReturnCar();
+                    InfoMsg.InnerText = "Car Returned";
+                    
+                }
+            }
+            catch (Exception Ex)
+            {
+                // throw;
+                InfoMsg.InnerText = Ex.Message;
+            }
+            DelayTb.Value = string.Empty;
+            FineTb.Value = string.Empty;
+        }
+    }
+}
